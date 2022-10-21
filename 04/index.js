5dcae250a75913cd5b9dac3b512cb658a425f300
okeypolilla <89881186+okeypolilla@users.noreply.github.com>
2022-10-21 16:51:08 -0300

    first commit

---
+++ 04/index.js
@@ -0,0 +1,6 @@
+var app=new Vue({
+    el: '#aplicacion',
+    data:{ 
+      lenguajes: ['C', 'C++', 'C#', 'Java', 'Ruby', 'Kotlin']
+    }
+  })
---
+++ 05/index.js
@@ -0,0 +1,19 @@
+var app=new Vue({
+    el: '#aplicacion',
+    data:{ 
+      articulos: [{
+                  codigo: 1, 
+                  descripcion: 'papas',
+                  precio: 12.52
+                 },{
+                  codigo: 2, 
+                  descripcion: 'naranjas',
+                  precio: 21
+                 },{
+                  codigo: 3, 
+                  descripcion: 'peras',
+                  precio: 18.20
+                 }]
+
+    }
+  })
